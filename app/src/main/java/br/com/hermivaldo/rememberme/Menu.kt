1ca7a5a9bdaa2e029029fbf665ef01a36565cc11
hermivaldo <f782241@mail.caixa>
2019-01-10 11:27:26 -0200

    Estrutura e protótipo do projeto

---
+++ app/src/main/java/br/com/hermivaldo/rememberme/Menu.kt
@@ -0,0 +1,12 @@
+package br.com.hermivaldo.rememberme
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Menu : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
